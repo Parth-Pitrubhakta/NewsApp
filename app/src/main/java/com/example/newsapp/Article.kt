cb2727d007942f8911ebf4f33d7bc6f0ea7dde93
Parth Pitrubhakta <parthwani1990@gmail.com>
2022-12-18 03:26:22 +0530

    Added DAO setup

--- app/src/main/java/com/example/newsapp/Article.kt
+++ app/src/main/java/com/example/newsapp/Article.kt
@@ -1,6 +1,14 @@
 package com.example.newsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
+
 data class Article(
+    @PrimaryKey(autoGenerate = true)
     val author: String,
     val content: String,
     val description: String,
