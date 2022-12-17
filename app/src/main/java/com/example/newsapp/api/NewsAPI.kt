74c4e8b6bf02f100eea0f2159ce87b2bfc36cd65
Parth Pitrubhakta <parthwani1990@gmail.com>
2022-12-18 03:12:46 +0530

    Added Retrofit Setup

---
+++ app/src/main/java/com/example/newsapp/api/NewsAPI.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.NewsResponse
+import com.example.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-technology")
+    suspend fun getTechnologyNews(
+        @Query("country")
+        countryCode: String = "in",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "74e849925a9a47e5aaac9f546056ae48"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
